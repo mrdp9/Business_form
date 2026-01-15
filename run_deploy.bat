2cf9deb34b374a5a7baacbbf91277f31ee1e33a4
mrdp9 <mridulpandey999e@gmail.com>
2026-01-15 06:30:53 +0000

    removed nodemodules

--- run_deploy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Running docker info... > deployment_log.txt
-docker info >> deployment_log.txt 2>&1
-echo. >> deployment_log.txt
-echo Running docker compose build and up... >> deployment_log.txt
-docker compose -f docker-compose.yaml up -d --build >> deployment_log.txt 2>&1
-echo Done. >> deployment_log.txt
